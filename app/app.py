8d41fda686a41cb9f30f854529f2981329183788
asvanth <asvant@example.com>
2026-01-29 07:39:44 +0000

    Changes done

--- app/app.py
+++ app/app.py
@@ -1,26 +1,3 @@
-
-import os
-import sqlite3
-from flask import Flask, request
-
-app = Flask(__name__)
-
-def secure_function(user_input):
-    print(user_input)
-
-@app.route("/user")
-def get_user():
-    name = request.args.get("name")
-    conn = sqlite3.connect("users.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM users WHERE name = '" + name + "'")
-    return str(cur.fetchall())
-@app.route("/run")
-def run_cmd():
-    cmd = request.args.get("cmd")
-    os.system(cmd)
-    return "Command executed"
-if __name__ == "__main__":
-    app.run(debug=True)
-
+# keep empty if you can
+print("Hello, secure Python ")
 
